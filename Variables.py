7028571e6797948dbce8145a59e4ae7e875338d7
zaina1479 <abuhamdanzaina@gmail.com>
2026-02-18 20:39:23 +0300

    Create Variables.py

---
+++ Variables.py
@@ -0,0 +1,17 @@
+#Variable
+# Syntax => [Variable Name] [Assignment Operator] [Value]
+
+# Name Convention and Rules
+# [1] Can Start With (a-z A-Z) Or Underscore
+# [2] You Can't start With Num Or Special Characters
+# [3] Can Include (0-9) Or Underscore
+# [4] Can't Include Special Characters
+# [5] Name is Not Like name [ Case Sensitive ]
+
+name = "Zaina Abu-Hamdan"  # Single Word => Normal
+myName = "Zaina Abu-Hamdan"  # Two Words => camelCase
+my_name = "Zaina Abu-Hamdan"  # Two Words => snake_case
+
+print(name)
+print(myName)
+print(my_name)
